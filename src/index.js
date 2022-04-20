079a2bc95781f66120ebffce8e6c651ddb555777
Monijo <mokajozefowicz@gmail.com>
2022-04-20 19:35:58 +0200

    Add pretteier to project.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-const Pet = (props) =>{
-    return React.createElement(
-        "div", {}, [
-            React.createElement("h2", {}, props.name),
-            React.createElement("h3", {}, props.animal),
-            React.createElement("h3", {}, props.breed),
-        ]
-    )
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
-const App = () =>{
-    return React.createElement(
-        "div",
-        {},
-        [React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "dog",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"
-            })
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+  ]);
+};
 
-
-
-        ]
-    )
-}
-
-const container = document.getElementById("app")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
