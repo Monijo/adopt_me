a61499d7811074f79969abbf694e3d89ee04da74
Monijo <mokajozefowicz@gmail.com>
2022-07-05 22:30:09 +0200

    add tests to custum hook

--- src/__tests__/useBreedList.test.js
+++ src/__tests__/useBreedList.test.js
@@ -3,5 +3,24 @@
  */
 
 import { expect, test } from "@jest/globals";
-import { act, render } from "@testing-library/react";
-import Carousel from "../components/Carousel";
+import { act, render, renderHook } from "@testing-library/react";
+
+import useBreedList from "../hooks/useBreedList";
+
+// function getBreedList(animal) {
+//   let list;
+//   function TestComponent() {
+//     list = useBreedList(animal);//tu sie da uzyc hooka
+//     return null;//ale zwraca tylko jsxa
+//   }
+//   render(<TestComponent />);
+//   return list; //zwraca dane, potzrebny wrapp
+// }
+
+test("gives an empty list with no animal", async () => {
+  const { result } = renderHook(() => useBreedList());
+
+  const [breedList, status] = result.current;
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("unloaded");
+});
